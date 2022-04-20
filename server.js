bf383836e642d7696bbb3c42c35c9e87c8a544cf
Kelly Lee <amorfati38@gmail.com>
2022-04-19 22:11:06 -0600

    deploying

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/good-spending', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: true
 });
 
 // routes
